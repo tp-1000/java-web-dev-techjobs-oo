c09bb5d9dbb1ee5659d194eb75550ac631c7bc94
thomaspetty <thomaskpetty@gmail.com>
2020-05-11 10:15:39 -0500

    Test Added

--- src/org/launchcode/techjobs_oo/Job.java
+++ src/org/launchcode/techjobs_oo/Job.java
@@ -45,6 +45,10 @@ public class Job {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,37 @@
+package org.launchcode.techjobs_oo.Tests;
+
+
+import org.junit.Test;
+import org.launchcode.techjobs_oo.*;
+
+import static org.junit.Assert.*;
+
+public class JobTest {
+
+    @Test
+    public void testSettingJobId() {
+        //Getting the Job class static nextId field is handled by sampling the current ID
+        int nextId = new Job().getId()+1;
+        for (int i = nextId; i < nextId+5; i++) {
+            assertEquals(i, new Job().getId());
+        }
+    }
+
+    @Test
+    public void testJobConstructorSetsCorrectly() {
+        Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(job.getName() instanceof String);
+        assertTrue(job.getEmployer() instanceof Employer);
+        assertTrue(job.getLocation() instanceof Location);
+        assertTrue(job.getPositionType() instanceof PositionType);
+        assertTrue(job.getCoreCompetency() instanceof CoreCompetency);
+    }
+
+    @Test
+    public void twoJobObjectsNotEqual() {
+        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertFalse(job1.equals(job2));
+    }
+}
