516379347ba65baba108f7757b84fdd70d0b1eed
thomaspetty <thomaskpetty@gmail.com>
2020-05-13 16:08:05 -0500

    refactored project to Object Oriented Design, classes added and updated

---
+++ src/org/launchcode/techjobs_oo/JobField.java
@@ -0,0 +1,42 @@
+package org.launchcode.techjobs_oo;
+
+import java.util.Objects;
+
+public abstract class JobField {
+    private int id;
+    private static int nextId = 1;
+    private String value = "Data not available";
+
+
+    public JobField() {
+        id = nextId++;
+    }
+
+    public JobField(String value) {
+        this();
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()){
+            return false;
+        }
+        JobField that = (JobField) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {return Objects.hash(id);}
+
+    @Override
+    public String toString() {return value;}
+
+    public int getId() {return id;}
+
+    public String getValue() {return value;}
+
+    public void setValue(String value) {this.value = value;}
+
+}
